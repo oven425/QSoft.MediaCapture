1c198883049f46738785ec8e56fdffac1d2b3aca
oven425 <oven425@yahoo.com.tw>
2024-10-09 22:47:27 +0800

    update
    1. study mediacapture again

--- MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Maui.Core;
 using CommunityToolkit.Mvvm.ComponentModel;
+using System.Runtime.Versioning;
 
 namespace MauiApp1
 {
@@ -9,15 +10,23 @@ namespace MauiApp1
         {
             InitializeComponent();
         }
+        public CancellationToken Token => CancellationToken.None;
+        private async void button_snapshot_Clicked(object sender, EventArgs e)
+        {
+            await this.cameraview.CaptureImage(Token);
+        }
+
+        int m_ImageCount = 1;
+        private void cameraview_MediaCaptured(object sender, CommunityToolkit.Maui.Views.MediaCapturedEventArgs e)
+        {
+            
+            var imagePath = Path.Combine(FileSystem.CacheDirectory, $"{m_ImageCount++}.jpg");
+            using var localFileStream = File.Create(imagePath);
+
+            e.Media.CopyTo(localFileStream);
+
+        }
     }
 
-    public partial class CameraViewVM(ICameraProvider cameraProvider) :ObservableObject
-    {
-        [ObservableProperty]
-        CameraInfo? selectedCamera;
-
-        [ObservableProperty]
-        Size selectedResolution;
-    }
 
 }
