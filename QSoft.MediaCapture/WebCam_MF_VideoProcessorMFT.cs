1048a2f4229b1419d3cf1978b3922411737ac53a
oven425 <oven425@yahoo.com.tw>
2024-11-20 08:56:05 +0800

    update
    1. fix resource leak when setprocesserMFC

--- QSoft.MediaCapture/WebCam_MF_VideoProcessorMFT.cs
+++ QSoft.MediaCapture/WebCam_MF_VideoProcessorMFT.cs
@@ -16,34 +16,41 @@ namespace QSoft.MediaCapture
         {
             m_VideoProcessMFT_Index = streamindex;
             IMFMediaType? pMediaType=null;
-            source.GetCurrentDeviceMediaType(streamindex, out pMediaType);
-
-            //CLSID_CColorControlDmo
-            var videoprocesstype = Type.GetTypeFromCLSID(DirectN.MFConstants.CLSID_VideoProcessorMFT);
-            this.m_VideoProcessor = Activator.CreateInstance(videoprocesstype) as IMFVideoProcessorControl;
-            HRESULT? hr = HRESULTS.S_OK;
-            if(this.m_Setting.IsMirror && m_VideoProcessor != null)
+            try
             {
-                //m_VideoProcessor.SetRotation(_MF_VIDEO_PROCESSOR_ROTATION.ROTATION_NORMAL);
-                //hr = m_VideoProcessor?.SetRotationOverride(90);
-                hr = m_VideoProcessor?.SetMirror(_MF_VIDEO_PROCESSOR_MIRROR.MIRROR_HORIZONTAL);
+                source.GetCurrentDeviceMediaType(streamindex, out pMediaType);
+                var videoprocesstype = Type.GetTypeFromCLSID(DirectN.MFConstants.CLSID_VideoProcessorMFT);
+                this.m_VideoProcessor = Activator.CreateInstance(videoprocesstype) as IMFVideoProcessorControl;
+
+                HRESULT hr;
+                if (this.m_Setting.IsMirror && m_VideoProcessor != null)
+                {
+                    hr = m_VideoProcessor.SetMirror(_MF_VIDEO_PROCESSOR_MIRROR.MIRROR_HORIZONTAL);
+                    if (hr != HRESULTS.S_OK) return Task.FromResult(hr);
+                }
+
+
+                if (m_VideoProcessor is IMFTransform mft)
+                {
+                    hr = mft.SetInputType(0, pMediaType, 0);
+                    if (hr != HRESULTS.S_OK) return Task.FromResult(hr);
+                    hr = mft.SetOutputType(0, pMediaType, 0);
+                    if (hr != HRESULTS.S_OK) return Task.FromResult(hr);
+                }
+
+                m_TaskAddEffect = new();
+
+                hr = source.AddEffect(streamindex, m_VideoProcessor);
+                if (hr != HRESULTS.S_OK) return Task.FromResult(hr);
+                return m_TaskAddEffect.Task;
             }
-
-
-            //HRESULT hr;
-            if (m_VideoProcessor is IMFTransform mft)
+            finally
             {
-                hr = mft.SetInputType(0, pMediaType, 0);
-                hr = mft.SetOutputType(0, pMediaType, 0);
+                SafeRelease(pMediaType);
             }
-
-            m_TaskAddEffect = new TaskCompletionSource<HRESULT>();
-
-            hr = source.AddEffect(streamindex, m_VideoProcessor);
-            return m_TaskAddEffect.Task;
         }
-        TaskCompletionSource<HRESULT>? m_TaskRemoveAllEffect;
 
+        TaskCompletionSource<HRESULT>? m_TaskRemoveAllEffect;
         async Task<HRESULT> RemoveAllVideoProcessorMFT(IMFCaptureSource source)
         {
             m_TaskRemoveAllEffect = new();
